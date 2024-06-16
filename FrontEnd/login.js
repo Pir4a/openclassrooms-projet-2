11c5cb01e8cbe760c8f04c3c44ea779af07c4d31
Pi4a <piralith@outlook.fr>
2024-06-16 18:46:21 +0200

    login fonctionnel

---
+++ FrontEnd/login.js
@@ -0,0 +1,35 @@
+const loginForm = document.getElementById("loginform")
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  let form = {
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  }
+  console.log(
+    JSON.stringify({
+      email: form.email,
+      password: form.password,
+    })
+  )
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      accept: "application/json",
+    },
+    body: JSON.stringify({
+      email: form.email,
+      password: form.password,
+    }),
+  }).then((response) => {
+    if (response.status !== 200) {
+      alert("Email ou mot de passe erronés")
+    } else {
+      response.json().then((data) => {
+        sessionStorage.setItem("token", data.token)
+        window.location.replace("index.html")
+      })
+    }
+  })
+})
